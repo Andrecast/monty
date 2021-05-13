3d4cdc318cd06e5de798cb3272cc8ef648894795
mareizaa <marce.3210@gmail.com>
2021-05-13 16:31:43 -0500

    up

--- monty.h
+++ monty.h
@@ -7,6 +7,7 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <string.h>
+#include <errno.h>
 
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
@@ -17,6 +18,7 @@
  * Description: doubly linked list node structure
  * for stack, queues, LIFO, FIFO Holberton project
  */
+extern char valor; 
 typedef struct stack_s
 {
         int n;
@@ -37,13 +39,14 @@ typedef struct instruction_s
         char *opcode;
         void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
-ssize_t read_textfile(const char *filename, size_t letters);
+int read_textfile(const char *filename, size_t letters);
 char **tock_arg(char *buff_arg);
 int count_w(char *buff);
-int count_2(char *buff);
+int count_2(char *buff, char *id);
 char **tock_2(char *buff_arg);
 void (*funtions(char *words))(stack_t **stack, unsigned int line_number);
 void _push(stack_t **stack, unsigned int line_number);
 void _pall(stack_t **stack, __attribute__((unused))unsigned int line_number);
+int free_dpointer(char **pointer);
 
 #endif
