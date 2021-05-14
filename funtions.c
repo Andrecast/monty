8f251a1a19d1d08e79b518d64a917c9cb867fe2b
mareizaa <marce.3210@gmail.com>
2021-05-13 21:18:41 -0500

    fu

--- funtions.c
+++ funtions.c
@@ -12,20 +12,18 @@ void funtions(char *words, stack_t **stack, unsigned int line_number)
 		{"swap", _swap},
 		{"add", _add},
 		{NULL, NULL}
-	}; 
-	printf("%d", i);
+	};
 	for (i = 0; op[i].opcode != NULL; i++)
 	{
-		printf("hola");
 		if (strcmp(words, op[i].opcode) == 0)
 		{
 			op[i].f(stack, line_number);
+			break;
 		}
 	}
-	printf("%d", i);
+
 	if (op[i].opcode == NULL)
 	{
-		printf("%s", words);
-		//dprintf(2, "L<line_number>: unknown instruction %s\n", words);
+		dprintf(2, "L<line_number>: unknown instruction %s\n", words);
 	}
 }
