5a79049ed31b4c514034b539db99d4698162a1c4
mareizaa <marce.3210@gmail.com>
2021-05-14 13:34:14 -0500

    betty

--- main.c
+++ main.c
@@ -1,17 +1,19 @@
 #include "monty.h"
 /**
  * main - check the code for Holberton School students.
- *
+ * @ac: length array.
+ * @av: double pointer strings
  * Return: Always 0.
  */
 int main(int ac, char **av)
 {
-    size_t buff = 500;
-    if (ac != 2)
-    {
-        dprintf(2, "USAGE: monty file\n");
-        exit(1);
-    }
-    read_textfile(av[1], buff);
-    return (0);
+	size_t letters = 500;
+
+	if (ac != 2)
+	{
+		dprintf(2, "USAGE: monty file\n");
+		exit(1);
+	}
+	read_textfile(av[1], buff);
+	return (0);
 }
