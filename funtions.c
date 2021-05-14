39ec6141d8087285bd13078b23d5d19932ca3aa3
mareizaa <marce.3210@gmail.com>
2021-05-13 19:14:48 -0500

    f

--- funtions.c
+++ funtions.c
@@ -1,30 +1,31 @@
 #include "monty.h"
 
-void (*funtions (char *words))(stack_t **stack, unsigned int line_number)
+void funtions(char *words, stack_t **stack, unsigned int line_number)
 {
-	instruction_t op_code[] = {
-		{"push", _push},
-		{"pall", _pall},
-		/*{"pint", _pint},
-		{"pop", _pop},
-		{"swap", _swap},
-		{"add", _add},*/
-		{NULL, NULL}
-	};
 	int i = 0;
 
-	i++;
-	while (op_code[i].opcode != NULL)
+	instruction_t op [] = {
+		{"push", _push},
+		{"pall", _pall},
+		{"pint", _pint},
+		{"pop", _pop},
+		{"swap", _swap},
+		{"add", _add},
+		{NULL, NULL}
+	}; 
+	printf("%d", i);
+	for (i = 0; op[i].opcode != NULL; i++)
 	{
-		if (strcmp(words, op_code[i].opcode) == 0)
+		printf("hola");
+		if (strcmp(words, op[i].opcode) == 0)
 		{
-			return (op_code[i].f);
+			op[i].f(stack, line_number);
 		}
-		i++;
 	}
-	if (op_code[i].opcode == NULL)
+	printf("%d", i);
+	if (op[i].opcode == NULL)
 	{
-		dprintf(2, "L<line_number>: unknown instruction %s\n", words);
+		printf("%s", words);
+		//dprintf(2, "L<line_number>: unknown instruction %s\n", words);
 	}
-	return ('\0');
 }
